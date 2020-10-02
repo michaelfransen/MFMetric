ab3dbf3487513ecb6ecf57be6e6acb267fd68fc0
Michael Fransen <michaelfransen@Michaels-MacBook-Air.local>
2020-10-02 16:35:16 -0700

    Orientation Updated

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 #define CHIPSET             WS2812B
 
 // initial matrix layout (to get led strip index by x/y)
-#define MATRIX_WIDTH        8
-#define MATRIX_HEIGHT       32
-#define MATRIX_TYPE         HORIZONTAL_ZIGZAG_MATRIX
+#define MATRIX_WIDTH        32
+#define MATRIX_HEIGHT       8
+#define MATRIX_TYPE         VERTICAL_ZIGZAG_MATRIX
 #define MATRIX_SIZE         (MATRIX_WIDTH*MATRIX_HEIGHT)
 #define NUMPIXELS           MATRIX_SIZE
 
@@ -22,18 +22,17 @@ cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
 void setup() {
   // initial FastLED by using CRGB led source from our matrix class
   FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds[0], leds.Size()).setCorrection(TypicalSMD5050);
-  FastLED.setBrightness(100);
+  FastLED.setBrightness(40);
   FastLED.clear(true);
 }
 
 void loop() {
   EVERY_N_SECONDS(1) {
-    //leds.DrawPixel(7, 31, CRGB::Cyan);
+    //leds.DrawPixel(31, 7, CRGB::Cyan);
     leds.DrawFilledRectangle(0, 0, 7, 7, CRGB::DarkCyan);
-    leds.DrawFilledRectangle(0, 8, 7, 15, CRGB::Orange);
-    leds.DrawFilledRectangle(0, 16, 7, 23, CRGB::DarkCyan);
-    leds.DrawFilledRectangle(0, 24, 7, 31, CRGB::Orange);
-  
+    leds.DrawFilledRectangle(8, 0, 15, 7, CRGB::Orange);
+    leds.DrawFilledRectangle(16, 0, 23, 7, CRGB::DarkCyan);
+    leds.DrawFilledRectangle(24, 0, 31, 7, CRGB::Orange);
     FastLED.show();
   }
 }
