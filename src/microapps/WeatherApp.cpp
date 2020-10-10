cd4462c70838c6af78532856ce829f27d562e30d
Michael Fransen <michaelfransen@Michaels-MacBook-Air.local>
2020-10-09 21:51:52 -0700

    retrieve and parse weather

--- src/microapps/WeatherApp.cpp
+++ src/microapps/WeatherApp.cpp
@@ -3,9 +3,11 @@
 #include <MFFont.h>
 #include "taskmanager/header.hpp"
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 void retrieveWeather();
 HTTPClient http;
+double temp;
 
 Task weatherRetrieverTask(15 * TASK_MINUTE, TASK_FOREVER, &retrieveWeather);
 
@@ -15,15 +17,23 @@ WeatherApp::WeatherApp(cLEDMatrixBase *leds)
 }
 
 void retrieveWeather() {
-    http.begin("http://api.openweathermap.org/data/2.5/weather?zip=94115,US&appid=d134621845de557af90dd48dd7dfa1b1");
+    http.begin("http://api.openweathermap.org/data/2.5/weather?zip=94115,US&appid=d134621845de557af90dd48dd7dfa1b1&units=imperial");
     int statusCode = http.GET();
 
     if (statusCode > 0) { //Check for the returning code
         String payload = http.getString();
-        Serial.println(statusCode);
-        Serial.println(payload);
-      }
- 
+        StaticJsonDocument<3276> doc;
+        DeserializationError error = deserializeJson(doc, payload);
+
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.c_str());
+            return;
+        }
+
+        temp = doc["main"]["temp"];
+        Serial.println(temp);
+    }
     else {
       Serial.println("Error on HTTP request");
     }
